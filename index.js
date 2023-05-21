38f19d14d8e3147ffc6b86e7f2174a0de0bb220e
SumaiyaAK <sumaiyasrn6@gmail.com>
2023-05-21 20:45:19 +0600

    last comment and final check

--- index.js
+++ index.js
@@ -89,6 +89,8 @@ async function run() {
       
     })
 
+    // Delete 
+
     app.delete('/addToy/:id', async(req, res)=>{
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
